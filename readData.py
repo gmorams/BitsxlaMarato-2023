96a50921a3a87e1bfa94ab0ad5a04fe9bb9e2783
polsahi <psaumell100@gmail.com>
2023-12-15 22:02:48 +0100

    imports

--- readData.py
+++ readData.py
@@ -1,7 +1,8 @@
 import pandas as pd
+from sklearn import svm
 
 df = pd.read_csv('data/Metadata_and_relative_abundance_of_seminal_microbiota_from_idiopathic_infertile_patients_and_donors.csv')
 
 
 
-print(df.head())
+print(df)
