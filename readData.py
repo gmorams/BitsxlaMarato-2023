9b35f6fc2998b62fdcf566c74d702288cff18434
polsahi <psaumell100@gmail.com>
2023-12-15 19:47:15 +0100

    primer csv llegit

--- readData.py
+++ readData.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('data.csv')
+df = pd.read_csv('data/Metadata_and_relative_abundance_of_seminal_microbiota_from_idiopathic_infertile_patients_and_donors.csv')
+
+
 
 print(df.head())
