c50e5d61d26e8fb11ecd3c2a3ed9f69bf9e1783a
peremateu <pere.mateu03@gmail.com>
2023-12-16 09:48:32 +0100

    Canvi noms

--- clustering.py
+++ clustering.py
@@ -1,14 +1,16 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.cluster import AgglomerativeClustering
+import numpy as np
+
+
 # from sklearn.datasets import make_blobs
 # from sklearn import metrics
 # from sklearn.decomposition import PCA
 # from sklearn import preprocessing
 # from sklearn.cluster import KMeans
-from sklearn.cluster import AgglomerativeClustering
 # import scipy.cluster.hierarchy as sch
-import numpy as np
 
 df = pd.read_csv('data/Metadata_and_relative_abundance_of_seminal_microbiota_from_idiopathic_infertile_patients_and_donors.csv')
 
