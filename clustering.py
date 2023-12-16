8ce134b58aae25bfa4012a0302c06149a5fc6777
peremateu <pere.mateu03@gmail.com>
2023-12-16 11:34:11 +0100

    DBSCAN clustering

--- clustering.py
+++ clustering.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import AgglomerativeClustering, DBSCAN
 import numpy as np
+from sklearn.neighbors import NearestNeighbors
 
 
 # from sklearn.datasets import make_blobs
@@ -12,10 +13,46 @@ import numpy as np
 # from sklearn.cluster import KMeans
 # import scipy.cluster.hierarchy as sch
 
-df = pd.read_csv('data/Metadata_and_relative_abundance_of_seminal_microbiota_from_idiopathic_infertile_patients_and_donors.csv')
+df = pd.read_csv('data/genus_level.csv')
+df.set_index('Sample ID', inplace=True)
 
 # OPCIÓ 1:
 # Fer clustering d'individus
 # Veure a quin grup pertanyen
 # 1. En cas que no apareguin dels 2 grups indescriminadament, cagada pastoret, més difícil de treure conclusions
 # 2. En cas que 
+
+def DBSCAN_clustering(dd):
+    n_clust = []
+    for i in range(1, 50):
+        clustering = DBSCAN(eps=i).fit(dd)
+        labels = clustering.labels_
+
+        # Number of clusters in labels, ignoring noise if present.
+        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+        n_noise_ = list(labels).count(-1)
+        n_clust.append(n_clusters_)
+    plt.plot(n_clust)
+    # plt.xlabel('Data Point Index')
+    # plt.ylabel('Epsilon (distance to kth nearest neighbor)')
+    plt.savefig('clusters')
+    # Max number of clusters: 4 at epsilon 22
+
+    clustering = DBSCAN(eps=25).fit(dd)
+    labels = clustering.labels_
+    
+    clusters = {'UAB': {i:0 for i in range(-1,4)}, 'CON': {i:0 for i in range(-1,4)}}
+    for i, label in enumerate(labels):
+        print(f"Index {dd.index[i]}: Cluster Label {label}")
+
+        clusters[dd.index[i][:3]][label] += 1
+    print('CLUSTERS UAB:',clusters['UAB'])
+    print('CLUSTERS CON:',clusters['CON'])
+
+            
+        
+
+
+
+
+DBSCAN_clustering(df)
