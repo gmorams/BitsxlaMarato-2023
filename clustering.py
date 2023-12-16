d97f5c6a862da69453ae23489761b535100a8802
peremateu <pere.mateu03@gmail.com>
2023-12-16 09:43:33 +0100

    transformacio csv

--- clustering.py
+++ clustering.py
@@ -1,12 +1,19 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_blobs
-from sklearn import metrics
-from sklearn.decomposition import PCA
-from sklearn import preprocessing
-from sklearn.cluster import KMeans
+# from sklearn.datasets import make_blobs
+# from sklearn import metrics
+# from sklearn.decomposition import PCA
+# from sklearn import preprocessing
+# from sklearn.cluster import KMeans
 from sklearn.cluster import AgglomerativeClustering
-import scipy.cluster.hierarchy as sch
+# import scipy.cluster.hierarchy as sch
+import numpy as np
 
 df = pd.read_csv('data/Metadata_and_relative_abundance_of_seminal_microbiota_from_idiopathic_infertile_patients_and_donors.csv')
+
+# OPCIÓ 1:
+# Fer clustering d'individus
+# Veure a quin grup pertanyen
+# 1. En cas que no apareguin dels 2 grups indescriminadament, cagada pastoret, més difícil de treure conclusions
+# 2. En cas que 
